446c5bb5f9ce15ab92135f1132e5c107588527ea
Tiberiu <tibi@qbyco.com>
2014-05-23 15:49:01 +0300

    Added a js test file. Update main runner

---
+++ recipes/ci-project/jobs/deploy-master.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+echo "[MASTER] GIT fetch init";
+
+git fetch;
+git checkout master;
+
+echo "[MASTER] GIT fetch success";
---
+++ recipes/ci-project/jobs/test_2.js
@@ -0,0 +1,3 @@
+#!/usr/bin/env node
+
+console.log("Qbyco CI test a js file");
